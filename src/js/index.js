0b223654eef8e6a474e3d49677172bb3d8c7ad7f
Henry Orama <henryorama07@gmail.com>
2018-10-24 22:14:27 +0000

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,31 @@
+import "../style/index.scss";
+
+window.onload = () => {
+  document.querySelector(".card").classList.add(generateRandomSuit());
+  document.querySelector(".card").innerHTML = generateRandomNumber();
+};
+
+let generateRandomNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
+let generateRandomSuit = () => {
+  let suit = ["Diamonds", "Spades", "Hearts", "Clubs"];
+  let indexSuit = Math.floor(Math.random() * suit.length);
+  return suit[indexSuit];
+};
